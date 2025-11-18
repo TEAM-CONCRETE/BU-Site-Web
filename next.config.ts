a4e174bcf8685d090650c4dc122d8123f54aa17d
박승희 <ridia05031@gmail.com>
2025-11-18 16:58:59 +0900

    [#17] refactor: 프록시 설정 보완

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
 import type { NextConfig } from "next";
 
 //임시로 samesite 설정을 위해 프록시 추가함
-const proxyTarget =
-  process.env.NEXT_PUBLIC_API_PROXY_TARGET?.replace(/\/$/, "") ?? null;
+const proxyTarget = process.env.API_PROXY_TARGET?.replace(/\/$/, "") ?? null;
 
 const nextConfig: NextConfig = {
   async rewrites() {
